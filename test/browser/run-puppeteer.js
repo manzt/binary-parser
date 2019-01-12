ec702d594ad6a7c76688e009eee2564a25095d55
Colin <colin.diesh@gmail.com>
2019-01-11 23:38:45 -0500

    Add webpack and browser test

---
+++ test/browser/run-puppeteer.js
@@ -0,0 +1,52 @@
+/* eslint-env node */
+
+const puppeteer = require("puppeteer");
+const path = require("path");
+
+let browser;
+return puppeteer
+  .launch()
+  .then(b => {
+    browser = b;
+    return b.newPage();
+  })
+  .then(
+    page =>
+      new Promise(async (resolve, reject) => {
+        page.on("console", msg => {
+          console.log(msg.text());
+        });
+        console.log('here')
+        console.log(`file:${path.join(__dirname, "run-mocha.html")}`)
+        await page.goto(`file:${path.join(__dirname, "run-mocha.html")}`);
+        return page
+          .evaluate(
+            () =>
+              new Promise((res, rej) => {
+                setInterval(() => {
+                  if (window.mocha.stats) {
+                    if (
+                      window.mocha.failures === 0 &&
+                      window.mocha.tests === window.mocha.passes
+                    ) {
+                      res();
+                    } else {
+                      rej();
+                    }
+                  }
+                }, 500);
+              })
+          )
+          .then(resolve, reject);
+      })
+  )
+  .then(
+    () => {
+      browser.close();
+      process.exit(0);
+    },
+    () => {
+      browser.close();
+      process.exit(1);
+    }
+  );
